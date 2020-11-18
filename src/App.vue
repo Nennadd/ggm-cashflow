ea197c4cd1685f7aa0a0f268e6e440ff39ceae2f
Nennadd <nenadj372@gmail.com>
2020-11-18 19:12:33 +0100

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <Header />
+    <div id="main">
+      <div id="nav">
+        <router-link to="/">Home</router-link>
+        <router-link to="/supplier-orders">Supplier Orders</router-link>
+      </div>
+      <router-view />
+    </div>
+  </div>
+</template>
+<script>
+import Header from "@/components/Header.vue";
+export default {
+  components: {
+    Header
+  }
+};
+</script>
+<style lang="scss">
+body {
+  padding: 0;
+  margin: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+#main {
+  display: grid;
+  grid-template-columns: 15rem 1fr;
+}
+#nav {
+  display: flex;
+  flex-direction: column;
+  padding: 2rem 1rem;
+  background: #444;
+
+  a {
+    display: block;
+    padding: 0.5rem 0;
+    font-weight: bold;
+    color: #eee;
+    text-decoration: none;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="header">
+    This is header
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style lang="scss" scoped>
+#header {
+  text-align: center;
+  background-color: #333;
+  color: #eee;
+  padding: 1rem 0;
+}
+</style>
