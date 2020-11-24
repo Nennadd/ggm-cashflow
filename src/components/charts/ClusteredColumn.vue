a321fd5d3f968ea891ea2985386de919d5b5a0b3
Nennadd <nenadj372@gmail.com>
2020-11-24 12:31:34 +0100

    Working on chart performance

--- src/components/charts/ClusteredColumn.vue
+++ src/components/charts/ClusteredColumn.vue
@@ -2,15 +2,13 @@
   <div class="chart" ref="chartdiv"></div>
 </template>
 <script>
-import * as am4core from "@amcharts/amcharts4/core";
-import * as am4charts from "@amcharts/amcharts4/charts";
-// import kelly from "@amcharts/amcharts4/themes/kelly";
-import am4themes_animated from "@amcharts/amcharts4/themes/animated";
-
-// am4core.useTheme(kelly);
-am4core.useTheme(am4themes_animated);
 export default {
   name: "ClusteredColumn",
+  props: {
+    AmChart: {
+      type: Object
+    }
+  },
   data() {
     return {
       loading: false,
@@ -44,94 +42,109 @@ export default {
     };
   },
   mounted() {
-    let chart = am4core.create(this.$refs.chartdiv, am4charts.XYChart);
-    this.chart = chart;
-    chart.colors.step = 2;
+    (async () => {
+      this.AmChart.am4core.options.queue = true;
+      this.chart = await this.AmChart.am4core.create(
+        this.$refs.chartdiv,
+        this.AmChart.am4charts.XYChart
+      );
+      this.chart.responsive.enabled = true;
+      //   this.AmChart.am4core.options.queue = true;
+      this.chart.colors.step = 2;
 
-    chart.legend = new am4charts.Legend();
-    chart.legend.position = "top";
-    chart.legend.paddingBottom = 20;
-    chart.legend.labels.template.maxWidth = 95;
+      this.chart.legend = await new this.AmChart.am4charts.Legend();
+      this.chart.legend.position = "top";
+      this.chart.legend.paddingBottom = 20;
+      this.chart.legend.labels.template.maxWidth = 95;
 
-    var xAxis = chart.xAxes.push(new am4charts.CategoryAxis());
-    xAxis.dataFields.category = "category";
-    xAxis.renderer.cellStartLocation = 0.1;
-    xAxis.renderer.cellEndLocation = 0.9;
-    xAxis.renderer.grid.template.location = 0;
+      var xAxis = await this.chart.xAxes.push(
+        new this.AmChart.am4charts.CategoryAxis()
+      );
+      xAxis.dataFields.category = "category";
+      xAxis.renderer.cellStartLocation = 0.1;
+      xAxis.renderer.cellEndLocation = 0.9;
+      xAxis.renderer.grid.template.location = 0;
 
-    var yAxis = chart.yAxes.push(new am4charts.ValueAxis());
-    yAxis.min = 0;
+      var yAxis = await this.chart.yAxes.push(
+        new this.AmChart.am4charts.ValueAxis()
+      );
+      yAxis.min = 0;
 
-    function createSeries(value, name) {
-      var series = chart.series.push(new am4charts.ColumnSeries());
-      series.dataFields.valueY = value;
-      series.dataFields.categoryX = "category";
-      series.name = name;
+      const createSeries = async (value, name) => {
+        var series = await this.chart.series.push(
+          new this.AmChart.am4charts.ColumnSeries()
+        );
+        series.dataFields.valueY = value;
+        series.dataFields.categoryX = "category";
+        series.name = name;
 
-      series.events.on("hidden", arrangeColumns);
-      series.events.on("shown", arrangeColumns);
+        await series.events.on("hidden", arrangeColumns);
+        await series.events.on("shown", arrangeColumns);
 
-      var bullet = series.bullets.push(new am4charts.LabelBullet());
-      bullet.interactionsEnabled = false;
-      bullet.dy = 30;
-      bullet.label.text = "{valueY}";
-      bullet.label.fill = am4core.color("#ffffff");
+        var bullet = await series.bullets.push(
+          new this.AmChart.am4charts.LabelBullet()
+        );
+        bullet.interactionsEnabled = false;
+        bullet.dy = 30;
+        bullet.label.text = "{valueY}";
+        bullet.label.fill = this.AmChart.am4core.color("#ffffff");
 
-      return series;
-    }
+        return await series;
+      };
 
-    chart.data = this.data;
+      this.chart.data = await this.data;
 
-    createSeries("first", "The First");
-    createSeries("second", "The Second");
-    createSeries("third", "The Third");
+      await createSeries("first", "The First");
+      await createSeries("second", "The Second");
+      await createSeries("third", "The Third");
 
-    function arrangeColumns() {
-      var series = chart.series.getIndex(0);
+      const arrangeColumns = async () => {
+        var series = this.chart.series.getIndex(0);
 
-      var w =
-        1 -
-        xAxis.renderer.cellStartLocation -
-        (1 - xAxis.renderer.cellEndLocation);
-      if (series.dataItems.length > 1) {
-        var x0 = xAxis.getX(series.dataItems.getIndex(0), "categoryX");
-        var x1 = xAxis.getX(series.dataItems.getIndex(1), "categoryX");
-        var delta = ((x1 - x0) / chart.series.length) * w;
-        if (am4core.isNumber(delta)) {
-          var middle = chart.series.length / 2;
+        var w =
+          1 -
+          xAxis.renderer.cellStartLocation -
+          (1 - xAxis.renderer.cellEndLocation);
+        if (series.dataItems.length > 1) {
+          var x0 = xAxis.getX(series.dataItems.getIndex(0), "categoryX");
+          var x1 = xAxis.getX(series.dataItems.getIndex(1), "categoryX");
+          var delta = ((x1 - x0) / this.chart.series.length) * w;
+          if (this.AmChart.am4core.isNumber(delta)) {
+            var middle = this.chart.series.length / 2;
 
-          var newIndex = 0;
-          chart.series.each(function(series) {
-            if (!series.isHidden && !series.isHiding) {
-              series.dummyData = newIndex;
-              newIndex++;
-            } else {
-              series.dummyData = chart.series.indexOf(series);
-            }
-          });
-          var visibleCount = newIndex;
-          var newMiddle = visibleCount / 2;
+            var newIndex = 0;
+            await this.chart.series.each(series => {
+              if (!series.isHidden && !series.isHiding) {
+                series.dummyData = newIndex;
+                newIndex++;
+              } else {
+                series.dummyData = this.chart.series.indexOf(series);
+              }
+            });
+            var visibleCount = newIndex;
+            var newMiddle = visibleCount / 2;
 
-          chart.series.each(function(series) {
-            var trueIndex = chart.series.indexOf(series);
-            var newIndex = series.dummyData;
+            await this.chart.series.each(series => {
+              var trueIndex = this.chart.series.indexOf(series);
+              var newIndex = series.dummyData;
 
-            var dx = (newIndex - trueIndex + middle - newMiddle) * delta;
+              var dx = (newIndex - trueIndex + middle - newMiddle) * delta;
 
-            series.animate(
-              { property: "dx", to: dx },
-              series.interpolationDuration,
-              series.interpolationEasing
-            );
-            series.bulletsContainer.animate(
-              { property: "dx", to: dx },
-              series.interpolationDuration,
-              series.interpolationEasing
-            );
-          });
+              series.animate(
+                { property: "dx", to: dx },
+                series.interpolationDuration,
+                series.interpolationEasing
+              );
+              series.bulletsContainer.animate(
+                { property: "dx", to: dx },
+                series.interpolationDuration,
+                series.interpolationEasing
+              );
+            });
+          }
         }
-      }
-    }
+      };
+    })();
   },
   beforeDestroy() {
     if (this.chart) {
@@ -144,5 +157,9 @@ export default {
 .chart {
   padding: 0.5rem;
   box-shadow: 4px 4px 8px #aaa, -4px -4px 8px #fff;
+
+  &:hover {
+    box-shadow: 7px 7px 12px #aaa, -7px -7px 12px #fff;
+  }
 }
 </style>
