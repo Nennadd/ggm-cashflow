888fee4bf978e5582f97592c78d929a2208cf34b
Nennadd <nenadj372@gmail.com>
2020-11-24 09:29:31 +0100

    loading component added

---
+++ src/components/Loading.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="loading">
+    <h1>Loading...</h1>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+export default {
+  name: "Loading",
+  computed: {
+    ...mapGetters({ loading: "getLoading" })
+  },
+  methods: {
+    ...mapActions(["removeLoading"])
+  },
+  //   data() {
+  //     return {
+  //       isLoaded: false
+  //     };
+  //   },
+  //   inject: ["loading"],
+  mounted() {
+    document.onreadystatechange = () => {
+      if (document.readyState === "complete") {
+        this.removeLoading();
+      }
+    };
+  }
+};
+</script>
+
+<style>
+.loading {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #eaebf3;
+  z-index: 99;
+}
+</style>
