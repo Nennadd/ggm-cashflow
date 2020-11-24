4ef528a19dfe7f4aec5ecf9f8d45c635d245a431
Nennadd <nenadj372@gmail.com>
2020-11-24 12:33:15 +0100

    Loading state in Vuex

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,21 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    loading: true,
+  },
+  mutations: {
+    showLoading(state, payload) {
+      state.loading = payload;
+    },
+  },
+  actions: {
+    showLoading(state) {
+      state.commit("showLoading", false);
+    },
+  },
+  modules: {},
+  getters: {
+    getLoading: (state) => state.loading,
+  },
 });
