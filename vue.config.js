0022e95ad67ada286c6f82ac6c352b867ebd4844
Nennadd <nenadj372@gmail.com>
2020-11-24 09:28:01 +0100

    Vue config added

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  chainWebpack: (config) => {
+    config.plugin("prefetch").tap(() => {
+      return [
+        {
+          rel: "prefetch",
+          include: "asyncChunks",
+          fileBlacklist: [
+            /\.map$/,
+            /pdfmake\.[^.]+\.js$/,
+            /xlsx\.[^.]+\.js$/,
+            /fabric[^.]*\.[^.]+\.js$/,
+            /responsivedefaults\.[^.]+\.js$/,
+          ],
+        },
+      ];
+    });
+  },
+};
